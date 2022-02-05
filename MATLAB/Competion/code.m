b07e440d0201b3598efca6f2224e229f7a14c139
Pierre Pérez <pierre.perez@pucp.edu.pe>
2022-02-05 16:47:42 -0500

    upd

--- MATLAB/Competion/code.m
+++ MATLAB/Competion/code.m
@@ -19,5 +19,18 @@ fprintf("Espera a que se abra la aplicación, puede demorar segundos")
 data= removevars(data, {'id'});
 writetable(data,'wids.csv');
 %%
-regressionLearner
-
+%regressionLearner
+%%
+clear
+load ('dataTrained.mat')
+%Cargamos la data a entrenar
+testData=readtable("test.csv");
+%Eliminamos algunas columnas
+testData= removevars(testData, {'State_Factor', 'building_class','facility_type','direction_max_wind_speed','direction_peak_wind_speed','max_wind_speed','days_with_fog'});
+%Movemos la columna id
+testData = movevars(testData, 'id', 'Before', 'Year_Factor');
+predictedData=trainedModel.predictFcn(testData);
+tempMatrix=readmatrix("sample_solution.csv");
+tempMatrix(:,2)=predictedData;
+%%
+writematrix(tempMatrix,"solution.csv")
