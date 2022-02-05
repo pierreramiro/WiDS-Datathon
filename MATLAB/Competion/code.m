653d42a7e229e7aea3ca758708864118e90c486e
Pierre Pérez <pierre.perez@pucp.edu.pe>
2022-02-05 15:14:03 -0500

    upd

--- MATLAB/Competion/code.m
+++ MATLAB/Competion/code.m
@@ -1,6 +1,23 @@
-clc
 %% Importamos,analizamos y limpiamos
+clc
 %Lo hacemos con este siguiente comando, evitamos usar la herramienta de MATLAB
-data=readtable("test.csv");
+data=readtable("train.csv");
 %analizamos la data
 summary(data)
+%%
+%eliminamos caracteres y columnas innecesarias
+data= removevars(data, {'State_Factor', 'building_class','facility_type','direction_max_wind_speed','direction_peak_wind_speed','max_wind_speed','days_with_fog'});
+%"promediamos la data faltante"
+data.energy_star_rating = fillmissing(data.energy_star_rating, 'linear');
+data.year_built = fillmissing(data.year_built, 'makima');
+%Movemos la columna de "enfoque" al final
+data = movevars(data, 'site_eui', 'After', 'id');
+data = movevars(data, 'id', 'Before', 'Year_Factor');
+%% Entrenamos el modelo
+clc
+fprintf("Espera a que se abra la aplicación, puede demorar segundos")
+data= removevars(data, {'id'});
+writetable(data,'wids.csv');
+%%
+regressionLearner
+
