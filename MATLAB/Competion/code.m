1cea60865bc1d78737990e64951c780a69a56399
Pierre Pérez <pierre.perez@pucp.edu.pe>
2022-02-16 20:28:00 -0500

    upd

--- MATLAB/Competion/code.m
+++ MATLAB/Competion/code.m
@@ -2,26 +2,29 @@
 clc
 %Lo hacemos con este siguiente comando, evitamos usar la herramienta de MATLAB
 data=readtable("train.csv");
-%%
-%analizamos la data
+% analizamos la data
+data=rmmissing(data, 'DataVariables', 'energy_star_rating');
+data=rmmissing(data, 'DataVariables', 'year_built');
 summary(data)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %eliminamos caracteres y columnas innecesarias
 %data= removevars(data, {'State_Factor', 'building_class','facility_type','direction_max_wind_speed','direction_peak_wind_speed','max_wind_speed','days_with_fog'});
 data= removevars(data,{'direction_max_wind_speed','direction_peak_wind_speed','max_wind_speed','days_with_fog'});
-
 %"promediamos la data faltante"
 data.energy_star_rating = fillmissing(data.energy_star_rating, 'pchip');
 data.year_built = fillmissing(data.year_built, 'makima');
 %Movemos la columna de "enfoque" al final
 data = movevars(data, 'site_eui', 'After', 'id');
 data = movevars(data, 'id', 'Before', 'Year_Factor');
-testIdx = 1:3:398; %Usaremos aprox el 20% de la data para testear
+%eliminamos un 10%
+percentToDelete=10;
+testIdx = 1:100/percentToDelete:height(data);
 data(testIdx, : ) = [];
-%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data= removevars(data, {'id'});
 %Entrenamos el modelo
 clc
 fprintf("Espera a que se abra la aplicación, puede demorar segundos")
-data= removevars(data, {'id'});
 writetable(data,'wids.csv');
 %%
 regressionLearner
