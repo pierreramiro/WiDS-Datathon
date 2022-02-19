b1bf61c0554d02a33289ed309694b5dc14e34353
Pierre Pérez <pierre.perez@pucp.edu.pe>
2022-02-18 20:27:08 -0500

    upd

--- MATLAB/Competion/code.m
+++ MATLAB/Competion/code.m
@@ -18,15 +18,23 @@ data.year_built = fillmissing(data.year_built, 'makima');
 %Movemos la columna de "enfoque" al final
 data = movevars(data, 'site_eui', 'After', 'id');
 data = movevars(data, 'id', 'Before', 'Year_Factor');
-%eliminamos un 10%
-percentToDelete=10;
-testIdx = 1:100/percentToDelete:height(data);
-data(testIdx, : ) = [];
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%eliminamos un 10%
+%percentToDelete=10;
+%testIdx = 1:100/percentToDelete:height(data);
+%data(testIdx, : ) = [];
 data= removevars(data, {'id'});
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Solo nos quedamos con las columnas que deseamos
+data=data(:,{'facility_type','energy_star_rating','year_built','floor_area','State_Factor','site_eui'});
+%Agregamos la nueva columna, mezcla de palabras
+lastCol=data(:,{'facility_type','energy_star_rating','year_built','State_Factor','site_eui'});
+lastCol=string(lastCol{:,1})+"_"+string(lastCol{:,2})+"_"+string(lastCol{:,3})+"_"+string(lastCol{:,4});
+lastCol=array2table(lastCol,'VariableNames',{'mix_features'});
+data=[data lastCol];
+data = movevars(data, 'site_eui', 'After', 'mix_features');
 %Entrenamos el modelo
 clc
-fprintf("Espera a que se abra la aplicación, puede demorar segundos")
+fprintf("Espera a que se abra la aplicación, puede demorar segundos\n")
 writetable(data,'wids.csv');
 %%
 clc
